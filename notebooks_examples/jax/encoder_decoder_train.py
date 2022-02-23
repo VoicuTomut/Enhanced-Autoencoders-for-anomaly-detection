c6b5c31573f735a49c6a0b9c3dacbc42bafdf5d1
Michal Baczyk <michal.baczyk123@gmail.com>
2022-02-22 20:33:25 -0800

    Deleted all files from jax and replaced it with the newest version.

--- notebooks_examples/jax/encoder_decoder_train.py
+++ notebooks_examples/jax/encoder_decoder_train.py
@@ -18,12 +18,24 @@ np.random.seed(42)
 num_trash_bits = 5
 num_data_bits = 1
 num_entangler_bits = 0
-num_layers = 4
+num_layers = 1
 train_digits_each = 10
 test_digits_each = 30
-steps = 1000
-batch = 3
+steps = 10
+batch = 2 * train_digits_each
 lr = 0.01
+
+PARAMETERS = {
+    "Trash Bits": num_trash_bits,
+    "Data Bits": num_data_bits,
+    "EPR Pairs": num_entangler_bits // 2,
+    "Layers": num_layers,
+    "Training Digits Each": train_digits_each,
+    "Testing Digits Each": test_digits_each,
+    "Training Steps": steps,
+    "Batch Size": batch,
+    "Learning Rate": lr,
+}
 
 num_wires = num_trash_bits + num_data_bits
 trash_bits_encoding = list(range(num_trash_bits))
@@ -49,11 +61,15 @@ X_test = jnp.concatenate(
 )
 y_test = jnp.array([[1] * test_digits_each, [2] * test_digits_each]).ravel()
 
-num_weights = 2 * (
+num_weights = (
         num_layers * (num_wires + num_entangler_bits // 2) * 2 * 2 + num_trash_bits * 2
 )
-weights = np.random.uniform(0, 2 * np.pi, num_weights, requires_grad=True)
-weights = jnp.array(weights)
+
+weights_encoder = np.random.uniform(-np.pi, np.pi, num_weights, requires_grad=True)
+weights_encoder = jnp.array(weights_encoder)
+
+weights_decoder = np.random.uniform(-np.pi, np.pi, num_weights, requires_grad=True)
+weights_decoder = jnp.array(weights_decoder)
 
 
 def layer(theta, x, trash, data, entangler, pattern):
@@ -127,6 +143,157 @@ def U(theta0, x, trash, data, entangler, pattern):
         layer(theta0[l], x, trash, data, entangler, pattern)
 
 
+pattern_encode_0 = get_pattern(
+    trash_bits_encoding, data_bits_encoding + entangler_bits_encoding[::2]
+)
+
+dev0 = qml.device("default.qubit.jax", wires=num_wires + num_entangler_bits, shots=None)
+zero_state = jnp.zeros([2 ** num_trash_bits])
+zero_state = zero_state.at[0].set(1)
+
+
+@jax.jit
+@qml.qnode(dev0, interface="jax")
+def circuit_encoding(params, x, state):
+    x = jnp.array([x] * num_wires)
+    theta0 = params[: -num_trash_bits * 2].reshape([num_layers, 2, -1])
+    theta1 = params[-num_trash_bits * 2:].reshape([1, -1])
+
+    qml.AmplitudeEmbedding(state, wires=range(num_wires), normalize=True)
+
+    make_entangled_bits(entangler_bits_encoding)
+
+    U(
+        theta0,
+        x,
+        trash_bits_encoding,
+        data_bits_encoding,
+        entangler_bits_encoding[::2],
+        pattern_encode_0,
+    )
+
+    qml.broadcast(
+        qml.RY,
+        wires=range(num_trash_bits),
+        pattern="single",
+        parameters=jnp.array(
+            [
+                theta1[0][j] * x[i] + theta1[0][j + 1]
+                for i, j in zip(range(num_trash_bits), range(0, 2 * num_trash_bits, 2))
+            ]
+        ),
+    )
+    return qml.density_matrix(wires=range(num_trash_bits))
+
+
+def encoding_fidelity(params, x, state):
+    trash = circuit_encoding(params, x, state)
+    fid = jnp.dot(jnp.conj(zero_state), jnp.dot(trash, zero_state))
+    return jnp.real(fid)
+
+
+def cost_encoding(params, X, Y):
+    return -jnp.mean(jax.vmap(encoding_fidelity, in_axes=[None, 0, 0])(params, Y, X))
+
+
+def fit(params, optimizer):
+    opt_state = optimizer.init(params)
+
+    @jax.jit
+    def step(params, opt_state, X, Y):
+        loss_value, grads = jax.value_and_grad(cost_encoding)(params, X, Y)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss_value
+
+    costs_train = []
+    costs_test = []
+    for i in range(steps):
+        subset = np.random.choice(list(range(len(X_train))), batch, replace=False)
+        x_batch = X_train[subset]
+        y_batch = y_train[subset]
+        params, opt_state, loss_value = step(params, opt_state, x_batch, y_batch)
+        costs_train.append(-loss_value)
+
+    return params, costs_train, costs_test
+
+
+"""
+fig, ax = qml.draw_mpl(circuit_encoding)(weights_encoder, y_train[0], X_train[0])
+fig.show()
+"""
+print(
+    -cost_encoding(weights_encoder, X_train, y_train),
+    "encoder fidelity before train - train data",
+)
+print(
+    -cost_encoding(weights_encoder, X_test, y_test),
+    "encoder fidelity before train - test data",
+)
+
+optimizer = optax.adam(lr)
+t1 = time.time()
+(
+    weights_encoder,
+    costs_train,
+    costs_test,
+) = fit(weights_encoder, optimizer)
+
+plt.plot(costs_train, c="r")
+# plt.yscale('log')
+plt.title("Trash fidelity")
+plt.show()
+
+print(time.time() - t1, "seconds - training time")
+
+
+def get_fid_scores_encoded(params, X, Y):
+    return jax.vmap(encoding_fidelity, in_axes=[None, 0, 0])(params, Y, X)
+
+
+fid_scores_train_encoded = get_fid_scores_encoded(weights_encoder, X_train, y_train)
+fid_scores_test_encoded = get_fid_scores_encoded(weights_encoder, X_test, y_test)
+
+print(
+    jnp.mean(fid_scores_train_encoded),
+    "encoder fidelity after train - train data",
+)
+print(
+    jnp.mean(fid_scores_test_encoded),
+    "encoder fidelity after train - test data",
+)
+plt.scatter(
+    y=fid_scores_train_encoded[:train_digits_each],
+    x=range(train_digits_each),
+    marker="*",
+    c="r",
+)
+plt.scatter(
+    y=fid_scores_train_encoded[train_digits_each:],
+    x=range(train_digits_each, 2 * train_digits_each),
+    marker="+",
+    c="r",
+)
+plt.scatter(
+    y=fid_scores_test_encoded[:test_digits_each],
+    x=range(2 * train_digits_each, 2 * train_digits_each + test_digits_each),
+    marker="*",
+    c="b",
+)
+plt.scatter(
+    y=fid_scores_test_encoded[test_digits_each:],
+    x=range(
+        2 * train_digits_each + test_digits_each,
+        2 * train_digits_each + 2 * test_digits_each,
+    ),
+    marker="+",
+    c="b",
+)
+plt.legend(["Train 0", "Train 1", "Test 0", "Test 1"], loc=3)
+plt.title("Trash fidelity after encoder")
+plt.show()
+print("*" * 50)
+
 dev1 = qml.device("default.qubit.jax", wires=num_wires, shots=None)
 
 
@@ -158,12 +325,10 @@ pattern_decode = get_pattern(
 @qml.qnode(dev2, interface="jax")
 def circuit_decoding(params, x, state):
     x = jnp.array([x] * num_wires)
-    params0 = params[: num_weights // 2]
-    params1 = params[num_weights // 2:]
-    theta0_encode = params0[: -num_trash_bits * 2].reshape([num_layers, 2, -1])
-    theta1_encode = params0[-num_trash_bits * 2:].reshape([1, -1])
-    theta0_decode = params1[: -num_trash_bits * 2].reshape([num_layers, 2, -1])
-    theta1_decode = params1[-num_trash_bits * 2:].reshape([1, -1])
+    theta0_encode = weights_encoder[: -num_trash_bits * 2].reshape([num_layers, 2, -1])
+    theta1_encode = weights_encoder[-num_trash_bits * 2:].reshape([1, -1])
+    theta0_decode = params[: -num_trash_bits * 2].reshape([num_layers, 2, -1])
+    theta1_decode = params[-num_trash_bits * 2:].reshape([1, -1])
 
     qml.AmplitudeEmbedding(
         state, wires=trash_bits_encoding + data_bits_decoding, normalize=True
@@ -226,9 +391,12 @@ def fid_score(params, X, Y):
     return -jnp.mean(jax.vmap(check_fidelity, in_axes=[None, 0, 0])(params, Y, X))
 
 
-cost_pre = fid_score(weights, X_train, y_train)
-print(-cost_pre, "fidelity before train - train data")
-print(-fid_score(weights, X_test, y_test), "fidelity before train - test data")
+cost_pre = fid_score(weights_decoder, X_train, y_train)
+print(-cost_pre, "decoder fidelity before train - train data")
+print(
+    -fid_score(weights_decoder, X_test, y_test),
+    "decoder fidelity before train - test data",
+)
 
 
 def fit(params, optimizer):
@@ -256,12 +424,13 @@ def fit(params, optimizer):
 
 optimizer = optax.adam(lr)
 t1 = time.time()
-weights, costs_train, costs_test = fit(weights, optimizer)
+weights_decoder, costs_train, costs_test = fit(weights_decoder, optimizer)
 
 # plt.plot(costs_test)
 plt.plot(costs_train)
 # plt.legend(['Train', 'Test'])
 # plt.yscale('log')
+plt.title("Decoder Fidelity")
 plt.show()
 
 print(time.time() - t1, "seconds - training time")
@@ -271,19 +440,43 @@ def get_fid_scores(params, X, Y):
     return jax.vmap(check_fidelity, in_axes=[None, 0, 0])(params, Y, X)
 
 
-fid_scores_train = get_fid_scores(weights, X_train, y_train)
-fid_scores_test = get_fid_scores(weights, X_test, y_test)
-print(jnp.mean(fid_scores_train), "average fidelity after decoding - train data")
-print(jnp.mean(fid_scores_test), "average fidelity after decoding - test data")
+fid_scores_train = get_fid_scores(weights_decoder, X_train, y_train)
+fid_scores_test = get_fid_scores(weights_decoder, X_test, y_test)
+print(jnp.mean(fid_scores_train), "decoeder fidelity after decoding - train data")
+print(jnp.mean(fid_scores_test), "decoder fidelity after decoding - test data")
 
-plt.scatter(y=fid_scores_train[:train_digits_each], x=range(train_digits_each), marker='*', c='r')
-plt.scatter(y=fid_scores_train[train_digits_each:], x=range(train_digits_each), marker='+', c='r')
-plt.scatter(y=fid_scores_test[:test_digits_each], x=range(test_digits_each), marker='*', c='b')
-plt.scatter(y=fid_scores_test[test_digits_each:], x=range(test_digits_each), marker='+', c='b')
-plt.legend(['Train 0', 'Train 1', 'Test 0', 'Test 1'])
+plt.scatter(
+    y=fid_scores_train[:train_digits_each],
+    x=range(train_digits_each),
+    marker="*",
+    c="r",
+)
+plt.scatter(
+    y=fid_scores_train[train_digits_each:],
+    x=range(train_digits_each, 2 * train_digits_each),
+    marker="+",
+    c="r",
+)
+plt.scatter(
+    y=fid_scores_test[:test_digits_each],
+    x=range(2 * train_digits_each, 2 * train_digits_each + test_digits_each),
+    marker="*",
+    c="b",
+)
+plt.scatter(
+    y=fid_scores_test[test_digits_each:],
+    x=range(
+        2 * train_digits_each + test_digits_each,
+        2 * train_digits_each + 2 * test_digits_each,
+    ),
+    marker="+",
+    c="b",
+)
+plt.title("Final fidelity -Decoded state with initial state")
+plt.legend(["Train 0", "Train 1", "Test 0", "Test 1"], loc=3)
 plt.show()
-
 """
-fig, ax = qml.draw_mpl(circuit_decoding)(weights, y_train[0], X_train[0])
+fig, ax = qml.draw_mpl(circuit_decoding)(weights_decoder, y_train[0], X_train[0])
 fig.show()
 """
+print(PARAMETERS)
